054d9ba12ce713d984a8ebe8444a671223a2145a
AaronRamales <99706675+ronnyyjay@users.noreply.github.com>
2024-03-01 14:22:58 +0800

    Update MQuizContract.sol

--- MQuizContract.sol
+++ MQuizContract.sol
@@ -12,28 +12,25 @@ uint public totalSalary;
 
 function setAge(uint _age) public {
 age = _age;
-}
-
+  }
 
 function setHrsWrk(uint _hoursWorked) public {
 hoursWorked = _hoursWorked;
-}
-
+  }
 
 function setRate(uint _hourlyRate) public onlyOwner {
 require(_hourlyRate > 0, "Hourly rate must be greater than zero.");
 hourlyRate = _hourlyRate;
-}
-
+  }
 
 function calculateTotSal() public onlyOwner {
 require(hourlyRate > 0 && hoursWorked > 0, "Cannot calculate salary with zero values.");
 totalSalary = hourlyRate * hoursWorked;
-}
+  }
 
 // Restrict functions to the contract owner
 modifier onlyOwner() {
 require(msg.sender == tx.origin, "Only the contract owner can call this function.");
 _;
-}
-}
+    }
+  }
